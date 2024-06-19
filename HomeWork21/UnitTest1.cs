4ff9f7773f05f9b070a6f537a446f9a33d927ed5
matveykylak <matveykylak@gmail.com>
2024-06-19 19:18:40 +0300

    Add project files.

---
+++ HomeWork21/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HomeWork21
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
